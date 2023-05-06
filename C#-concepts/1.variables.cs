60e7393903fe9c0fd1735b29addfcec1378a6d0e
tcvduc <caovanducs@gmail.com>
2023-05-06 18:33:35 +0700

    onboard c sharp

---
+++ C#-concepts/1.variables.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CSharpVariables
+{
+    class StringInCSharp
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "this is a string";
+            Console.WriteLine(s1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CSharpVariables;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("I'm learning C#!");
+            Console.WriteLine("It is awesome!");
+            Console.WriteLine(3 + 3);
+
+            var s = new StringInCSharp();
+            s.ToString();
+
+        }
+    }
+}
